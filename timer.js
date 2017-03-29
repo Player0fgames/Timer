4ac1053358b2a9be98c344b7376084f0e8b87741
Player0fgames <m0rmegil@hotmail.com>
2017-03-29 15:53:33 -0600

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,1 +1,4 @@
-//Testing//
+//Collection of useful scraps
+var wGoal = prompt("How much time do you want to spend working this week?");
+var dGoal = prompt("How much time do you want to spend working each day?");
+confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
