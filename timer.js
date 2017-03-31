da11f4cab8aae621059243000004e4e624ba2b21
Player0fgames <m0rmegil@hotmail.com>
2017-03-31 15:29:58 -0600

    Update and rename timer.js to timer.html

--- timer.js
+++
@@ -1,4 +0,0 @@
-//Collection of useful scraps
-var wGoal = prompt("How much time do you want to spend working this week?");
-var dGoal = prompt("How much time do you want to spend working each day?");
-confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
